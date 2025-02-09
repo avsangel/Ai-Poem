affc26db2f88b04481eefe439b2cfb8c6c2018c6
Alesia <alesianicoleg@gmail.com>
2025-02-08 23:56:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("sup");
+function generatePoem(event) {
+  event.preventDefault;
+
+  alert("generating poem");
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
