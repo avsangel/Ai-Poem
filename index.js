8295f9de8690bfa86bf9202a19d039e4175813a6
Alesia <alesianicoleg@gmail.com>
2025-02-09 00:44:05 -0600

    instructions

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "66e942064o99c86ea1f3a0t6dbadb31b";
   let context = "be polite and provide a very short answer";
-  let prompt = "write me a poem";
+  let prompt = `Generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
